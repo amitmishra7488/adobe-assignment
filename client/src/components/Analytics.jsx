a95e09be4d97c9c113acf5cfa9f15597509feca1
Amit Kumar <amitmishra74880@gmail.com>
2023-04-10 13:20:46 +0530

    .

--- client/src/components/Analytics.jsx
+++ client/src/components/Analytics.jsx
@@ -1,60 +1,61 @@
-import React, { useState,useEffect } from 'react'
-import { Box,Text } from "@chakra-ui/react"
+import React, { useState, useEffect } from 'react'
+import { Box, Text } from "@chakra-ui/react"
 
 export default function Analytics() {
-    const [totalPosts,setTotalPosts] = useState(0);
-    const [totalUsers,setTotalUsers] = useState(0);
+    const [totalPosts, setTotalPosts] = useState(0);
+    const [totalUsers, setTotalUsers] = useState(0);
 
     const displayPost = async () => {
         try {
-          const res = await fetch('https://adobe-assignment-three.vercel.app/analytics/posts/')
-          const data = await res.json();
-          console.log(data);
-          setTotalPosts(data);
+            const res = await fetch('https://adobe-assignment-three.vercel.app/analytics/posts/')
+            const data = await res.json();
+            console.log(data);
+            setTotalPosts(data);
         }
         catch (err) {
-          console.log(err);
+            console.log(err);
         }
-      }
+    }
 
-      const displayUsers = async () => {
+    const displayUsers = async () => {
         try {
-          const res = await fetch('https://adobe-assignment-three.vercel.app/analytics/users/')
-          const data = await res.json();
-          console.log(data);
-          setTotalUsers(data);
+            const res = await fetch('https://adobe-assignment-three.vercel.app/analytics/users/')
+            const data = await res.json();
+            console.log(data);
+            setTotalUsers(data);
         }
         catch (err) {
-          console.log(err);
+            console.log(err);
         }
-      }
+    }
 
-      useEffect(() => {
+    useEffect(() => {
         displayPost();
         displayUsers();
-      }, [])
+    }, [])
 
     return (
-        <Box mt="64px" textAlign="center">
-            <Box>
-                <Text fontSize={40}>Total Post</Text>
-                <Text>{totalPosts.length}</Text>
+        <Box mt="10%" textAlign="center">
+            <Box display="flex" w="80%" m="auto"gap="10%" justifyContent="center" boxShadow="rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px">
+                <Box>
+                    <Text fontSize={40} frontWeight="700" fontFamily="cursive" >Total Post</Text>
+                    <Text fontSize={25} frontWeight="500" fontFamily="cursive">{totalPosts.length}</Text>
+                </Box>
+
+
+                <Box>
+
+                    <Text fontSize={40} frontWeight="700" fontFamily="cursive" >Total User</Text>
+                    <Text fontSize={25} frontWeight="500" fontFamily="cursive">{totalUsers.length}</Text>
+                </Box>
             </Box>
 
-            <Box>
-                <Text>Top Five Post</Text>
+
+            {/* <Box>
+                <Text fontSize={40} >Top Five Post</Text>
+                <Text >Top Five User</Text>
                 <Text></Text>
-            </Box>
-
-            <Box>
-                <Text>Total User</Text>
-                <Text>{totalUsers.length}</Text>
-            </Box>
-
-            <Box>
-                <Text>Top Five User</Text>
-                <Text></Text>
-            </Box>
+            </Box> */}
 
         </Box>
     )
